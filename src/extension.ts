85695372f6e28ca3eec30c57e6d32f9640d8455b
mariamsayed1 <mariamsayed777@gmail.com>
2025-02-07 06:17:50 +0200

    updates in timeout detection

--- src/extension.ts
+++ src/extension.ts
@@ -256,13 +256,17 @@ function detectTimeoutIssues(ast: any, file: string): string[] {
     walkSimple(ast, {
         CallExpression(node) {
             if (node.callee.type === 'MemberExpression' && node.callee.object.type === 'Identifier' && node.callee.object.name === 'axios') {
-                const configArg = node.arguments[1];
-                if (configArg && configArg.type === 'ObjectExpression') {
-                    const hasTimeoutProp = configArg.properties.some((prop: any) =>
-                        prop.type === 'Property' && prop.key.type === 'Identifier' && prop.key.name === 'timeout'
-                    );
-                    if (hasTimeoutProp) {
-                        hasTimeout = true;
+                // const configArg = node.arguments[1];
+                // Check all arguments
+                for (const configArg of node.arguments) {
+                    if (configArg && configArg.type === 'ObjectExpression') {
+                        const hasTimeoutProp = configArg.properties.some((prop: any) =>
+                            prop.type === 'Property' && prop.key.type === 'Identifier' && prop.key.name === 'timeout'
+                        );
+                        if (hasTimeoutProp) {
+                            hasTimeout = true;
+                            break; // Exit loop once timeout is found
+                        }
                     }
                 }
             }
